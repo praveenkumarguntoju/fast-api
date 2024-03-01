1a78f837ae2817a2d44dfbff75352069d204e9d9
Praveen Kumar Guntoju <gpravin999@gmail.com>
2024-03-01 15:15:56 +0000

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,6 @@ async def test_read_main():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"Hello": "Worldujujbkjbkj"}
+
+
