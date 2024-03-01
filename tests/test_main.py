b89738e631090cfc0058bd9a04344c4f98d1ab63
Praveen Kumar Guntoju <gpravin999@gmail.com>
2024-03-01 15:11:28 +0000

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,8 +3,6 @@ from httpx import AsyncClient
 import pytest
 from app/app.main import app
 
-
-
 @pytest.mark.asyncio
 async def test_read_main():
     async with AsyncClient(app=app, base_url="http://test") as ac:
